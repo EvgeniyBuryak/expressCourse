22e5fe796b2e5e7172c8e1c08e040b0a186ab25e
Jhonah <buryakone@gmail.com>
2021-07-06 23:05:03 +0700

    реализовал второй вариант суммирование через Замыкания

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-function sum(a, b, c) {
+// Вариант 1
+function sum1(a, b, c) {
     return +a + +b + +c;
 }
 
-alert ( sum (5, 10, 3) );
+alert(sum1(5, 10, 3));
+
+// Вариант 2 
+function sum2(a) {
+    return function (b) {
+        return function (c) {
+            return a + b + c;
+        };
+    };
+}
+
+alert(sum2(5)(10)(3));
