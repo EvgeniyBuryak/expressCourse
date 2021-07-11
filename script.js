c224a37973f9395b78f43cf9cbcf18f0d40512b1
Jhonah <buryakone@gmail.com>
2021-07-11 13:23:18 +0700

    добавил 3 вариант и обновил 1,2 варианты

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-// Вариант 1
-function sum1(a, b, c) {
-    return +a + +b + +c;
-}
+function sum1(a, b, c) { return +a + +b + +c; }
+let sum11 = (a, b, c) => +a + +b + c;
 
-alert(sum1(5, 10, 3));
+//alert("1ый вар. с обычной функцией: " + sum1(5, 10, 3));
+//alert("1ый вар. со стрелочной функцией: " + sum1(5, 10, 3));
 
-// Вариант 2 
 function sum2(a) {
     return function (b) {
         return function (c) {
@@ -14,4 +12,48 @@ function sum2(a) {
     };
 }
 
-alert(sum2(5)(10)(3));
+function sum22(a) {
+    function func(b) {
+        func.currentSum += b;
+        return func;
+    }
+
+    func.currentSum = a;
+
+    func.toString = () => {
+        return func.currentSum;
+    };
+
+    return func;
+}
+
+//alert("2ый вар. с замыканием: " + sum2(5)(10)(3));
+//alert("2ый вар. с замыканием масштабируемый: " + sum22(5)(10)(3)(9));
+
+function sum3(a) {
+    function func(b) {
+        func.currentSum += b;
+        return func;
+    }
+
+    func.add = function (c) {
+        func.currentSum += c;
+        return this;
+    };
+
+    func.result = () => {
+        alert(func.toString());
+        return func.currentThis;
+    };
+
+    func.currentSum = a;
+    func.currentThis = func;
+
+    func.toString = () => {
+        return func.currentSum;
+    };
+
+    return func;
+}
+
+sum3(5)(10)(3).add(4).result().add(9).add(10).result();
