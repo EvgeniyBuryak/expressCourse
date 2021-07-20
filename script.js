52358451bdd0383010af93aa17a24738660a4da5
Jhonah <buryakone@gmail.com>
2021-07-20 18:01:50 +0700

    переписал код, чтобы одна функция поддерживала все способы

--- script.js
+++ script.js
@@ -1,59 +1,40 @@
-function sum1(a, b, c) { return +a + +b + +c; }
-let sum11 = (a, b, c) => +a + +b + c;
-
-//alert("1ый вар. с обычной функцией: " + sum1(5, 10, 3));
-//alert("1ый вар. со стрелочной функцией: " + sum1(5, 10, 3));
-
-function sum2(a) {
-    return function (b) {
-        return function (c) {
-            return a + b + c;
-        };
-    };
+function add() {
+    return function (a, b) {
+        return a + b;
+    }
 }
 
-function sum22(a) {
+function sum(...rest) {
     function func(b) {
-        func.currentSum += b;
+        func.current += b;
         return func;
     }
 
-    func.currentSum = a;
+    func.add = func;    
 
-    func.toString = () => {
-        return func.currentSum;
-    };
-
-    return func;
-}
-
-//alert("2ый вар. с замыканием: " + sum2(5)(10)(3));
-//alert("2ый вар. с замыканием масштабируемый: " + sum22(5)(10)(3)(9));
-
-function sum3(a) {
-    function func(b) {
-        func.currentSum += b;
-        return func;
-    }
-
-    func.add = function (c) {
-        func.currentSum += c;
+    func.result = function(f) {
+        let re = this.toString();
+        if (f instanceof Function) {
+            f(re);
+            return this;
+        }
+        alert(re);
         return this;
     };
 
-    func.result = () => {
-        alert(func.toString());
-        return func.currentThis;
-    };
+    func.current = rest.reduce(add());
 
-    func.currentSum = a;
-    func.currentThis = func;
-
-    func.toString = () => {
-        return func.currentSum;
+    func.toString = function() {
+        return this.current;
     };
 
     return func;
 }
 
-sum3(5)(10)(3).add(4).result().add(9).add(10).result();
+alert(sum(5, 10, 3));
+alert(sum(5)(10)(3));
+//alert(sum(5)(10, 3));
+sum(5)(10)(3).add(4).result().add(9).add(10).result(); //22 and 41
+sum(5)(10)(3).add(4).add(9).add(10).result((value) => {
+    alert(value);
+}); //41
