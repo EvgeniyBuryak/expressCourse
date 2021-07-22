4034f51b86f74dee2507dff8f3563a8aff97c195
Jhonah <buryakone@gmail.com>
2021-07-22 17:41:34 +0700

    реализация стрлочными фуе фуями

--- script.js
+++ script.js
@@ -1,46 +1,37 @@
-function add() {
-    return function (a, b) {
-        return a + b;
-    }
-}
-
-/*
 function sum(...rest) {
-    function func(b) {
-        func.current += b;
+
+    let current = rest.reduce((a, b) => a + b);
+
+    let func = (c) => {
+        current += c;
         return func;
-    }
-
-    func.add = func;    
-
-    func.result = function(f) {
-        let re = this.toString();
-        if (f instanceof Function) {
-            f(re);
-            return this;
-        }
-        alert(re);
-        return this;
     };
 
-    func.current = rest.reduce(add());
+    func.add = func;
 
-    func.toString = function() {
-        return this.current;
+    func.result = (f = alert) => {
+        f(current);
+        return func;
     };
+
+    func.toString = () => current;
 
     return func;
 }
 
-alert(sum(5, 10, 3)); //18
-alert(sum(5)(10)(3)); //18
-alert(sum(5)(10, 3)); //15 <- don't work
-sum(5)(10)(3).add(4).result().add(9).add(10).result(); //22 and 41
+alert(sum(5, 10, 3)); //18 
+alert(sum(5)(10)(3)); //18 
+sum(5)(10)(3).add(4).result().add(9).add(10).result(); //22 and 41    
 sum(5)(10)(3).add(4).add(9).add(10).result((value) => {
     alert(value);
 }); //41
-*/
 
+/*sum(5).add(10).result((value) => {
+    alert(value);
+});
+sum(5).result();
+*/
+/*
 function curry(func) {    
     return function curried(...args) {
         if (args.length >= func.length) {
@@ -53,32 +44,46 @@ function curry(func) {
     }
 }
 
-function sum(a, b, c) {
-    sum.current = a + b + c;
+function sum2(a, b, c) {
+    sum2.current = a + b + c;
 
-    sum.add = (d) => {
-        sum.current += d
-        return sum;
+    sum2.add = (d) => {
+        sum2.current += d
+        return sum2;
     };
 
-    sum.result = function (f) {
-        let re = this.toString();
+    sum2.result = (f) => {
+        let re = sum2.toString();
         (!f) ? alert(re) : f(re);
-        return this;
+        return sum2;
     };
 
-    sum.toString = function () {
-        return this.current;
+    sum2.toString = () => {
+        return sum2.current;
     };
 
-    return sum;
+    return sum2;
 }
 
-let curriedSum = curry(sum);
+let curriedSum = curry(sum2);
+*/
+//alert(curriedSum(1, 2, 3)); // 6, всё ещё можно вызывать нормально  
+//alert(curriedSum(1)(2, 3)); // 6, каррирование первого аргумента    
+//curriedSum(1)(2)(3).result().add(4).result(); // 10, каррирование всех аргументов 
+//curriedSum(5)(10)(3).add(4).add(9).add(10).result((value) => {
+    //alert(value);
+//}); //41
 
-alert(curriedSum(1, 2, 3)); // 6, всё ещё можно вызывать нормально  
-alert(curriedSum(1)(2, 3)); // 6, каррирование первого аргумента    
-alert(curriedSum(1)(2)(3).add(4).result()); // 10, каррирование всех аргументов
-curriedSum(5)(10)(3).add(4).add(9).add(10).result((value) => {
-    alert(value);
-}); //41
+/*function resultAlert(f = 0, str = this.toString()) {
+    (!f) ? alert(str) : f(str);
+    return this;
+}
+function myCurry() {
+    return function (f) {
+        let str = this.toString();
+        (!f) ? alert(func()) : f(str);
+        return this;
+    }
+}*/
+//let add = (a, b) => a + b;
+//alert(sum(5)(10, 3)); //15 <- don't work 
